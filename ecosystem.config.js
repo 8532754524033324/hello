6b0e0dde5def4ab16f52a58cb1d8072b61a19fee
8532754524033324 <kennsukea@gmail.com>
2024-12-01 08:50:07 +0000

    five rcommit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,24 @@
 module.exports = {
-  deploy : {
-    production : {
-      user : 'yoshimon852',
-      host : '34.97.159.191',
-      ref  : 'origin/main',
-      repo : 'https://github.com/8532754524033324/hello.git',
-      path : '/var/www/html/hello',
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'index.js', // 起動スクリプトを正しく指定
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'yoshimon852',
+      host: '34.97.159.191',
+      ref: 'origin/main',
+      repo: 'https://github.com/8532754524033324/hello.git',
+      path: '/var/www/html/hello',
       'pre-deploy-local': '',
-      'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'cd /var/www/html/hello/current && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
